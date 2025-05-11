d183cd7fb9ebbdcf1f8cb19003d032ff268a4e4f
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-05-11 12:54:46 -0500

    Init: 08-05-2025 - Inicialización.
    Inicialización general.

---
+++ src/main/java/com/eapiis/main/Apirfds220251Application.java
@@ -0,0 +1,11 @@
+package com.eapiis.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirfds220251Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Apirfds220251Application.class, args);
+	}
+}
---
+++ src/test/java/com/eapiis/main/Apirfds220251ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eapiis.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirfds220251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
